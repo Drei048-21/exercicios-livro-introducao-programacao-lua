a008857fa4e3e60861baf80cbf66c12a77364cc0
Drei04821 <159065549+Drei048-21@users.noreply.github.com>
2026-09-19 21:23:26 -0300

    Add temperature conversion comments in ex01.lua
    Add comments explaining the purpose of the script and the conversion formula.

--- cap02/exercicios/ex01.lua
+++ cap02/exercicios/ex01.lua
@@ -1,3 +1,8 @@
+--[[
+Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+A fórmula de conversão é F = (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+]]
+
 io.write("Digite a Temperatura em °C: ")
 local celsius = io.read("*n")
 
